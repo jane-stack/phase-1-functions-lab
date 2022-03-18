1b9e8fa85943191da9b45b2d5e561ae4c439fb2d
Tuyen Ha <tuyen_ha@outlook.com>
2022-03-18 11:12:04 -0600

    commit tree 113e15cf216476fcc6aeb5c81800b31cc23dba7c

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return (blocks - 42);
+    } else {
+        return (42 - blocks);
+    }
+}
+
+function distanceFromHqInFeet(distanceFromHqInBlocks) {
+    return (distanceFromHqInBlocks * 264);
+  };
